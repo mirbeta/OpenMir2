1e72268a63a607237a0ca1550f0295ca6d658983
john <nowme@outlook.com>
2021-11-29 19:14:20 +0800

    补充提交

---
+++ src/GameGate/Protocol.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+
+namespace GameGate
+{
+
+    public struct TSvrCmdPack
+    {
+        public double Flag;
+        public double SockID;
+        public ushort Seq;
+        public ushort Cmd;
+        public int GGSock;
+        public int DataLen;
+    }
+
+    public struct _tagCmdHeader
+    {
+        public double Header;
+        public ushort Cmd;
+        public ushort Cmd1;
+        public double Tail;
+    }
+
+    public struct TMagic
+    {
+        public char[] Reserved1;
+        public ushort MagicID;
+        public char[] Reserved2;
+        public ushort Delay;
+        public char[] Reserved3;
+    }
+
+    public struct TEnDeInfo
+    {
+        public double Head;
+        public ushort Cmd;
+        public ushort Cmd1;
+        public double Tail;
+    }
+
+    public class TDelayMsg
+    {
+        public int dwDelayTime;
+        public int nMag;
+        public int nCmd;
+        public int nDir;
+        public int nBufLen;
+        public byte[] pBuffer;
+    } 
+
+    public struct TPerIPAddr
+    {
+        public long IPaddr;
+        public int Count;
+    }
+
+    public struct TIPArea
+    {
+        public double Low;
+        public double High;
+    } 
+
+    public enum TBlockIPMethod
+    {
+        mDisconnect,
+        mBlock,
+        mBlockList
+    } // end TBlockIPMethod
+
+    public enum TPunishMethod
+    {
+        ptTurnPack,
+        ptDropPack,
+        ptNullPack,
+        ptDelaySend
+    } // end TPunishMethod
+
+    public enum TChatFilterMethod
+    {
+        ctReplaceAll,
+        ctReplaceOne,
+        ctDropconnect
+    } // end TChatFilterMethod
+
+    public enum TOverSpeedMsgMethod
+    {
+        ptSysmsg,
+        ptMenuOK
+    }
+
+    public enum TSockThreadStutas
+    {
+        stConnecting,
+        stConnected,
+        stTimeOut
+    } 
+}
+
+namespace Protocol.Units
+{
+    public class Protocol
+    {
+        public static bool g_fServiceStarted = false;
+        public const string _STR_GRID_INDEX = "网关";
+        public const string _STR_GRID_IP = "网关地址";
+        public const string _STR_GRID_PORT = "端口";
+        public const string _STR_GRID_CONNECT_STATUS = "连接状态";
+        public const string _STR_GRID_IO_SEND_BYTES = "发送";
+        public const string _STR_GRID_IO_RECV_BYTES = "接收";
+        public const string _STR_KEEP_ALIVE = "**";
+        public const string _STR_CMD_FILTER = "%s 此命令禁止使用！";
+        public const string _STR_LIB_MMSYSTEM = "winmm.dll";
+        public const string _STR_LIB_KERNEL32 = "kernel32.dll";
+        public const string _STR_CONFIG_FILE = ".\\Config.ini";
+        public const string _STR_BLOCK_FILE = ".\\BlockIPList.txt";
+        public const string _STR_BLOCK_AREA_FILE = ".\\BlockIPAreaList.txt";
+        public const string _STR_CHAT_FILTER_FILE = ".\\ChatFilter.txt";
+        public const string _STR_CHAT_CMD_FILTER_FILE = ".\\CharCmdFilter.txt";
+        public const string _STR_PUNISH_USER_FILE = ".\\PunishList.txt";
+        public const uint RUNGATECODE = 0xAA55AA55 + 0x00450045;
+        public const int FIRST_PAKCET_MAX_LEN = 254;
+        public const int MAGIC_NUM = 0128;
+        public const int DELAY_BUFFER_LEN = 1024;
+    }
+}
+
---
+++ src/SystemModule/Misc.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using SystemModule;
+
+namespace SystemModule
+{
+    public class Misc
+    {
+        public static string EncodeString(string Str)
+        {
+            byte[] EncBuf = new byte[4096];
+            var tempBuf = HUtil32.GetBytes(Str);
+            var buffLen = EncodeBuf(tempBuf, Str.Length, EncBuf);
+            string result = HUtil32.GetString(EncBuf, 0, buffLen);
+            return result;
+        }
+
+        public static string DecodeString(string Str)
+        {
+            byte[] EncBuf = new byte[4096];
+            var tempBuf = HUtil32.GetBytes(Str);
+            var buffLen = DecodeBuf(tempBuf, Str.Length, EncBuf);
+            string result = HUtil32.GetString(EncBuf, 0, buffLen);
+            return result;
+        }
+
+        public static int EncodeBuf(byte[] Buf, int Len, byte[] DstBuf)
+        {
+            int result;
+            byte temp;
+            byte c;
+            var no = 2;
+            byte remainder = 0;
+            byte bySeed = 0xAC;
+            byte byBase = 0x3C;
+            var pos = 0;
+            var dstPos = 0;
+            for (var i = 0; i < Len; i++)
+            {
+                c = (byte)(Buf[pos] ^ bySeed);
+                pos++;
+                if (no == 6)
+                {
+                    DstBuf[dstPos] = (byte)((c & 0x3F) + byBase);
+                    dstPos++;
+                    remainder = (byte)(remainder | ((c >> 2) & 0x30));
+                    DstBuf[dstPos] = (byte)(remainder + byBase);
+                    dstPos++;
+                    remainder = 0;
+                }
+                else
+                {
+                    temp = (byte)(c >> 2);
+                    DstBuf[dstPos] = (byte)(((temp & 0x3C) | (c & 0x3)) + byBase);
+                    dstPos++;
+                    remainder = (byte)((remainder << 2) | (temp & 0x3));
+                }
+                no = no % 6 + 2;
+            }
+            if (no != 2)
+            {
+                DstBuf[dstPos]= (byte)(remainder + byBase);
+                dstPos++;
+            }
+            result = dstPos;
+            DstBuf[dstPos + 1] = (byte)('\0');
+            return result;
+        }
+
+        public static int DecodeBuf(byte[] Buf, int Len, byte[] DstBuf)
+        {
+            int result = 0;
+            int CurCycleBegin;
+            byte temp;
+            byte remainder;
+            byte c;
+            var nCycles = Len / 4;
+            var nBytesLeft = Len % 4;
+            byte bySeed = 0xAC;
+            byte byBase = 0x3C;
+            var dstPos = 0;
+            for (var i = 0; i < nCycles; i++)
+            {
+                CurCycleBegin = i * 4;
+                remainder = (byte)((Buf[CurCycleBegin + 3]) - byBase);
+                temp = (byte)(Buf[CurCycleBegin] - byBase);
+                c = (byte)(((temp << 2) & 0xF0) | (remainder & 0x0C) | (temp & 0x3));
+                DstBuf[dstPos] = (byte)(c ^ bySeed);
+                dstPos++;
+                temp = (byte)((Buf[CurCycleBegin + 1]) - byBase);
+                c = (byte)(((temp << 2) & 0xF0) | ((remainder << 2) & 0x0C) | (temp & 0x3));
+                DstBuf[dstPos] = (byte)(c ^ bySeed);
+                dstPos++;
+                temp = (byte)(Buf[CurCycleBegin + 2] - byBase);
+                c =(byte)( temp | ((remainder << 2) & 0xC0));
+                DstBuf[dstPos] = (byte)(c ^ bySeed);
+                dstPos++;
+            }
+            if (nBytesLeft == 2)
+            {
+                remainder = (byte)(Buf[Len - 1] - byBase);
+                temp = (byte)(Buf[Len - 2] - byBase);
+                c = (byte)(((temp << 2) & 0xF0) | ((remainder << 2) & 0x0C) | (temp & 0x3));
+                DstBuf[dstPos] = (byte)(c ^ bySeed);
+                dstPos++;
+            }
+            else if (nBytesLeft == 3)
+            {
+                remainder = (byte)(Buf[Len - 1] - byBase);
+                temp =(byte) (Buf[Len - 3] - byBase);
+                c = (byte)(((temp << 2) & 0xF0) | (remainder & 0x0C) | (temp & 0x3));
+                DstBuf[dstPos] = (byte)(c ^ bySeed);
+                dstPos++;
+                temp = (byte)(Buf[Len - 2] - byBase);
+                c = (byte)(((temp << 2) & 0xF0) | ((remainder << 2) & 0x0C) | (temp & 0x3));
+                DstBuf[dstPos] = (byte)(c ^ bySeed);
+                dstPos++;
+            }
+            result = dstPos;
+            DstBuf[dstPos + 1] = (byte)'\0';
+            return result;
+        }
+    } 
+}
+
