5ea3905fc710974d3dda79d98943613499e31560
summer <nowme@Outlook.com>
2022-03-14 13:37:43 +0800

    常规优化

--- src/SystemModule/Packet/TUserEntry.cs
+++ src/SystemModule/Packet/TUserEntry.cs
@@ -40,14 +40,14 @@ namespace SystemModule.Packet
 
         protected override void WritePacket(BinaryWriter writer)
         {
-            writer.Write(sAccount.ToByte(sAccount.Length + 1, 11));
-            writer.Write(sPassword.ToByte(sPassword.Length + 1, 11));
-            writer.Write(sUserName.ToByte(sUserName.Length + 1,21));
-            writer.Write(sSSNo.ToByte(sSSNo.Length + 1, 15));
-            writer.Write(sPhone.ToByte(sPhone.Length + 1, 15));
-            writer.Write(sQuiz.ToByte(sQuiz.Length + 1, 21));
-            writer.Write(sAnswer.ToByte(sAnswer.Length + 1, 13));
-            writer.Write(sEMail.ToByte(sEMail.Length + 1, 41));
+            writer.Write(sAccount, 10);
+            writer.Write(sPassword, 10);
+            writer.Write(sUserName, 20);
+            writer.Write(sSSNo, 14);
+            writer.Write(sPhone, 14);
+            writer.Write(sQuiz, 20);
+            writer.Write(sAnswer, 12);
+            writer.Write(sEMail, 40);
         }
     }
 }
