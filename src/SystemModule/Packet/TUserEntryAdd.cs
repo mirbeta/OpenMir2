8bf14c03c347f5a67bf87954e79b6a942c1e7aa7
summer <nowme@Outlook.com>
2022-03-14 16:03:26 +0800

    LoginSvr:修复账号注册问题(需要同步更新客户端)

--- src/SystemModule/Packet/TUserEntryAdd.cs
+++ src/SystemModule/Packet/TUserEntryAdd.cs
@@ -1,8 +1,6 @@
-using System.IO;
-
 namespace SystemModule.Packet
 {
-    public class TUserEntryAdd : Packets
+    public class TUserEntryAdd
     {
         public string sQuiz2;
         public string sAnswer2;
@@ -10,38 +8,5 @@ namespace SystemModule.Packet
         public string sMobilePhone;
         public string sMemo;
         public string sMemo2;
-
-        public const int PacketSize = 135;
-
-        public TUserEntryAdd()
-        {
-
-        }
-
-        public TUserEntryAdd(byte[] buff)
-            : base(buff)
-        {
-            this.sQuiz2 = ReadPascalString(20);
-            this.sAnswer2 = ReadPascalString(12);
-            this.sBirthDay = ReadPascalString(10);
-            this.sMobilePhone = ReadPascalString(13);
-            this.sMemo = ReadPascalString(20);
-            this.sMemo2 = ReadPascalString(20);
-        }
-
-        protected override void ReadPacket(BinaryReader reader)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void WritePacket(BinaryWriter writer)
-        {
-            writer.Write(sQuiz2, 20);
-            writer.Write(sAnswer2, 12);
-            writer.Write(sBirthDay, 10);
-            writer.Write(sMobilePhone, 13);
-            writer.Write(sMemo, 20);
-            writer.Write(sMemo2, 20);
-        }
     }
 }
