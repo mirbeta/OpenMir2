515219da9d00f872d2e7b04283718e206cac24bc
john <nowme@outlook.com>
2022-09-05 14:34:58 +0800

    fixz

--- src/GameSvr/AppService.cs
+++ src/GameSvr/AppService.cs
@@ -40,7 +40,7 @@ namespace GameSvr
             return base.StartAsync(cancellationToken);
         }
 
-        public override Task StopAsync(CancellationToken cancellationToken)
+        public override async Task StopAsync(CancellationToken cancellationToken)
         {
             if (M2Share.UserEngine.PlayObjectCount > 0)
             {
@@ -55,14 +55,13 @@ namespace GameSvr
             //如果有多机负载转移在线玩家到新服务器
             var sIPaddr = string.Empty;
             var nPort = 0;
-
-            var isMultiServer = M2Share.GetMultiServerAddrPort(0, ref sIPaddr, ref nPort); //如果有可用服务器，那就切换过去
+            var isMultiServer = M2Share.GetMultiServerAddrPort(M2Share.ServerIndex, ref sIPaddr, ref nPort); //如果有可用服务器，那就切换过去
             if (isMultiServer)
             {
                 var playerCount = M2Share.UserEngine.PlayObjects.Count();
                 if (playerCount > 0)
                 {
-                    Task.Run(() =>
+                    await Task.Run(async () =>
                     {
                         var shutdownSeconds = 120;
                         while (playerCount > 0)
@@ -76,13 +75,13 @@ namespace GameSvr
                             }
                             if (shutdownSeconds > 0)
                             {
-                                Thread.Sleep(TimeSpan.FromSeconds(1));
+                                await Task.Delay(TimeSpan.FromSeconds(1));
                             }
                             else
                             {
                                 foreach (var playObject in M2Share.UserEngine.PlayObjects)
                                 {
-                                    if (playObject.Ghost || playObject.Death) //死亡或者下线的玩家不进行转移
+                                    if (playObject.Ghost || playObject.Death)//死亡或者下线的玩家不进行转移
                                     {
                                         playerCount--;
                                         continue;
@@ -103,7 +102,6 @@ namespace GameSvr
                 M2Share.boStartReady = false;
                 _mirApp.Stop();
             }
-            return base.StopAsync(cancellationToken);
         }
     }
 }
