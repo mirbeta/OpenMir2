a88732b7dd0c63bd0e374df8e8d79ec00d61c29b
john <nowme@outlook.com>
2022-10-11 21:06:28 +0800

    修复MakePlayer账号注册

--- src/SystemModule/Packet/ClientPackets/UserEntryAdd.cs
+++ src/SystemModule/Packet/ClientPackets/UserEntryAdd.cs
@@ -1,6 +1,8 @@
+using System.IO;
+
 namespace SystemModule.Packet.ClientPackets
 {
-    public class UserEntryAdd
+    public class UserEntryAdd : Packets
     {
         public string sQuiz2;
         public string sAnswer2;
@@ -8,5 +10,15 @@ namespace SystemModule.Packet.ClientPackets
         public string sMobilePhone;
         public string sMemo;
         public string sMemo2;
+
+        protected override void ReadPacket(BinaryReader reader)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void WritePacket(BinaryWriter writer)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
