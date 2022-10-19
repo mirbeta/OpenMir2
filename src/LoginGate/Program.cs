bb41aa58c5112f77762e6c35f92a7d65ed86a61c
john <nowme@outlook.com>
2022-10-19 16:19:43 +0800

    GameSvr简单的清理

--- src/LoginGate/Program.cs
+++ src/LoginGate/Program.cs
@@ -11,6 +11,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
