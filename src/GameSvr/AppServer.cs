d712702a3c6ef8f5775a7c7782152435de7e221c
john <nowme@outlook.com>
2022-11-01 02:46:00 +0800

    refactor: gameSvr测试

---
+++ src/GameSvr/AppServer.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Spectre.Console;
+using System.Reflection;
+using System.Text;
+using SystemModule;
+using SystemModule.Hosts;
+
+namespace GameSvr
+{
+    public class AppServer : ServiceHost
+    {
+        private static PeriodicTimer _timer;
+        
+        public AppServer()
+        {
+            PrintUsage();
+
+            //Console.CancelKeyPress += delegate
+            //{
+            //    //GateShare.ShowLog = true;
+            //    if (_timer != null)
+            //    {
+            //        _timer.Dispose();
+            //    }
+            //    AnsiConsole.Reset();
+            //};
+
+            Builder.ConfigureLogging(ConfigureLogging);
+            Builder.ConfigureServices(ConfigureServices);
+            Builder.UseConsoleLifetime();
+            Builder.ConfigureHostOptions(options =>
+            {
+                options.ShutdownTimeout = TimeSpan.FromSeconds(30);
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MirLogger>();
+            services.AddSingleton<GameApp>();
+            services.AddHostedService<AppService>();
+            services.AddHostedService<TimedService>();
+        }
+
+        private void ConfigureLogging(ILoggingBuilder logging)
+        {
+            logging.ClearProviders();
+            logging.SetMinimumLevel(LogLevel.Trace);
+            logging.AddNLog(Configuration);
+        }
+
+        public override async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await Builder.RunConsoleAsync(cancellationToken);
+            //await ProcessLoopAsync();
+            //Stop();
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+        
+        static void Stop()
+        {
+            AnsiConsole.Status().Start("Disconnecting...", ctx =>
+            {
+                ctx.Spinner(Spinner.Known.Dots);
+            });
+        }
+
+        static void PrintUsage()
+        {
+            AnsiConsole.WriteLine();
+            using var logoStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GameSvr.logo.png");
+            var logo = new CanvasImage(logoStream!)
+            {
+                MaxWidth = 25
+            };
+
+            var table = new Table()
+            {
+                Border = TableBorder.None,
+                Expand = true,
+            }.HideHeaders();
+            table.AddColumn(new TableColumn("One"));
+
+            var header = new FigletText("OpenMir2")
+            {
+                Color = Color.Fuchsia
+            };
+            var header2 = new FigletText("Game Svrver")
+            {
+                Color = Color.Aqua
+            };
+
+            var sb = new StringBuilder();
+            sb.Append("[bold fuchsia]/s[/] [aqua]查看[/] 网关状况\n");
+            sb.Append("[bold fuchsia]/r[/] [aqua]重读[/] 配置文件\n");
+            sb.Append("[bold fuchsia]/c[/] [aqua]清空[/] 清除屏幕\n");
+            sb.Append("[bold fuchsia]/q[/] [aqua]退出[/] 退出程序\n");
+            var markup = new Markup(sb.ToString());
+
+            table.AddColumn(new TableColumn("Two"));
+
+            var rightTable = new Table()
+                .HideHeaders()
+                .Border(TableBorder.None)
+                .AddColumn(new TableColumn("Content"));
+
+            rightTable.AddRow(header)
+                .AddRow(header2)
+                .AddEmptyRow()
+                .AddEmptyRow()
+                .AddRow(markup);
+            table.AddRow(logo, rightTable);
+
+            AnsiConsole.Write(table);
+            AnsiConsole.WriteLine();
+        }
+        
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/GameSvr/Program.cs
+++ src/GameSvr/Program.cs
@@ -1,131 +1,17 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using NLog;
-using NLog.Extensions.Logging;
-using Spectre.Console;
-using System.Reflection;
 using System.Runtime;
 using System.Text;
-using SystemModule;
 
 namespace GameSvr
 {
     class Program
     {
-        private static PeriodicTimer _timer;
-        private static Logger _logger;
-        private static IHost _host;
-        private static readonly CancellationTokenSource cts = new CancellationTokenSource();
-
         static async Task Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             GCSettings.LatencyMode = GCSettings.IsServerGC ? GCLatencyMode.Batch : GCLatencyMode.Interactive;
-            
-            var config = new ConfigurationBuilder().Build();
-
-            _logger = LogManager.Setup()
-                .SetupExtensions(ext => ext.RegisterConfigSettings(config))
-                .GetCurrentClassLogger();
-
-            //PipeStream();
-
-            PrintUsage();
-
-            //Console.CancelKeyPress += delegate
-            //{
-            //    //GateShare.ShowLog = true;
-            //    if (_timer != null)
-            //    {
-            //        _timer.Dispose();
-            //    }
-            //    AnsiConsole.Reset();
-            //};
-
-            var builder = new HostBuilder()
-                .UseConsoleLifetime()
-                .ConfigureHostOptions(options =>
-                 {
-                     options.ShutdownTimeout = TimeSpan.FromSeconds(30);
-                 })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddSingleton<GameApp>();
-                    services.AddSingleton<MirLogger>();
-                    services.AddHostedService<AppService>();
-                    services.AddHostedService<TimedService>();
-                }).ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                    logging.AddNLog(config);
-                });
-
-
-            await builder.RunConsoleAsync(cts.Token);
-            //await ProcessLoopAsync();
-            //Stop();
+            var serviceRunner = new AppServer();
+            await serviceRunner.RunAsync();
         }
-
-        static void Stop()
-        {
-            AnsiConsole.Status().Start("Disconnecting...", ctx =>
-            {
-                ctx.Spinner(Spinner.Known.Dots);
-            });
-        }
-
-        static void PrintUsage()
-        {
-            AnsiConsole.WriteLine();
-            using var logoStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GameSvr.logo.png");
-            var logo = new CanvasImage(logoStream!)
-            {
-                MaxWidth = 25
-            };
-
-            var table = new Table()
-            {
-                Border = TableBorder.None,
-                Expand = true,
-            }.HideHeaders();
-            table.AddColumn(new TableColumn("One"));
-
-            var header = new FigletText("OpenMir2")
-            {
-                Color = Color.Fuchsia
-            };
-            var header2 = new FigletText("Game Svrver")
-            {
-                Color = Color.Aqua
-            };
-
-            var sb = new StringBuilder();
-            sb.Append("[bold fuchsia]/s[/] [aqua]查看[/] 网关状况\n");
-            sb.Append("[bold fuchsia]/r[/] [aqua]重读[/] 配置文件\n");
-            sb.Append("[bold fuchsia]/c[/] [aqua]清空[/] 清除屏幕\n");
-            sb.Append("[bold fuchsia]/q[/] [aqua]退出[/] 退出程序\n");
-            var markup = new Markup(sb.ToString());
-
-            table.AddColumn(new TableColumn("Two"));
-
-            var rightTable = new Table()
-                .HideHeaders()
-                .Border(TableBorder.None)
-                .AddColumn(new TableColumn("Content"));
-
-            rightTable.AddRow(header)
-                .AddRow(header2)
-                .AddEmptyRow()
-                .AddEmptyRow()
-                .AddRow(markup);
-            table.AddRow(logo, rightTable);
-
-            AnsiConsole.Write(table);
-            AnsiConsole.WriteLine();
-        }
-
     }
 }
