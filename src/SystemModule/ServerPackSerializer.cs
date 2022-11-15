5deda172b67f080eb2c56d6b01cdf005ec656ae6
john <nowme@outlook.com>
2022-11-16 01:14:48 +0800

    perf: 使用性能更快更高效的序列化组件

---
+++ src/SystemModule/ServerPackSerializer.cs
@@ -0,0 +1,19 @@
+using MemoryPack;
+using System.Runtime.CompilerServices;
+
+namespace SystemModule
+{
+    public class ServerPackSerializer
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte[] Serialize<T>(T origin)
+        {
+            return MemoryPackSerializer.Serialize(origin);
+        }
+
+        public T Deserialize<T>(byte[] bytes)
+        {
+            return MemoryPackSerializer.Deserialize<T>(bytes)!;
+        }
+    }
+}
