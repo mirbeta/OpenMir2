60a9b5238481a57e4becfb46cf6dadf951c5eecb
john <nowme@outlook.com>
2023-03-19 17:51:55 +0800

    refactor: market拍卖行更多实现

--- src/Storeages/DBSvr.Storage.MySQL/MarketStoageService.cs
+++ src/Storeages/DBSvr.Storage.MySQL/MarketStoageService.cs
@@ -1,19 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using SystemModule.Data;
+using SystemModule.Packets.ClientPackets;
 
 namespace DBSrv.Storage.MySQL
 {
     public class MarketStoageService : IMarketStorage
     {
+        private readonly StorageOption _storageOption;
+
         public MarketStoageService(StorageOption storageOption)
         {
-
+            _storageOption = storageOption;
         }
 
-        public IEnumerable<MarketItem> QueryMarketItems(byte serverGroupId)
+        public IEnumerable<MarketItem> QueryMarketItems(byte groupId)
         {
-            return new List<MarketItem>();
+            using var context = new StorageContext(_storageOption);
+            var success = false;
+            context.Open(ref success);
+            if (!success)
+            {
+                return new List<MarketItem>();
+            }
+            const string sSqlStringAll = "select a.*,b.* from marketItems a left JOIN marketitem b on a.Id=b.MarketId where a.SellState=1";
+            const string sSqlString = "select a.*,b.* from marketItems a left JOIN marketitem b on a.Id=b.MarketId where a.SellState=1 and GroupId=@GroupId";
+            var command = context.CreateCommand();
+            command.CommandText = groupId == 0 ? sSqlStringAll : sSqlString;
+            command.Parameters.AddWithValue("@GroupId", groupId);
+            using var dr = command.ExecuteReader();
+            var itemList = new List<MarketItem>();
+            if (dr.Read())
+            {
+                var marketItem = new MarketItem();
+                marketItem.Index = dr.GetInt32("SellIndex");
+                marketItem.SellWho = dr.GetString("SellName");
+                marketItem.SellPrice = dr.GetInt32("SellPrice");
+                marketItem.Selldate = dr.GetString("SellDate");
+                marketItem.SellState = dr.GetByte("SellState");
+                marketItem.Item = new ClientItem();
+                marketItem.Item.MakeIndex = dr.GetInt32("MaekeIndex");
+                marketItem.Item.Dura = dr.GetUInt16("Dura");
+                marketItem.Item.DuraMax = dr.GetUInt16("DuraMax");
+                for (int i = 0; i < 13; i++)
+                {
+                    marketItem.Item.Desc[i] = dr.GetByte($"Value{i}");
+                }
+                itemList.Add(marketItem);
+            }
+            dr.Close();
+            dr.Dispose();
+            return itemList;
         }
 
         public int QueryMarketItemsCount(byte serverGroupId)
