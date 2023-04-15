9250d027e3a3ae132331b4912fc48fb1a737825b
john <nowme@outlook.com>
2023-04-15 10:32:14 +0800

    refactor: 移动GameSrv宠物判变时间字段

--- src/GameSrv/Actor/BaseObject.cs
+++ src/GameSrv/Actor/BaseObject.cs
@@ -329,11 +329,6 @@ namespace GameSrv.Actor
         protected int PoisoningTick;
         protected int VerifyTick;
         /// <summary>
-        /// 怪物叛变时间
-        /// </summary>
-        public int MasterRoyaltyTick;
-        public int MasterTick;
-        /// <summary>
         /// 恢复血量和魔法间隔
         /// </summary>
         protected int AutoRecoveryTick;
@@ -478,7 +473,6 @@ namespace GameSrv.Actor
             PoisoningTick = HUtil32.GetTickCount();
             VerifyTick = HUtil32.GetTickCount();
             AutoRecoveryTick = HUtil32.GetTickCount();
-            MasterTick = 0;
             WalkSpeed = 1400;
             NextHitTime = 2000;
             HealthTick = 0;
